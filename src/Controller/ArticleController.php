03dbb0ca11903484761028e235acfa81c894f925
alexandrebersihand <a.bersihand@hotmail.fr>
2019-10-22 09:40:44 +0200

    création page show article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,9 +14,20 @@ class ArticleController extends AbstractController
     public function index(ArticleRepository $repository)
     {
         $articles = $repository->findAll();
-        
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id":"\d+"}, methods={"GET"})
+     */
+    public function show(string $id, ArticleRepository $repository)
+    {
+        $article = $repository->find($id);
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
