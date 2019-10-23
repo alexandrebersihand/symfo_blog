d438c568ec9836c68aca37c2bd920db3f0207602
alexandrebersihand <a.bersihand@hotmail.fr>
2019-10-23 12:45:02 +0200

    factorisation template include

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,17 +1,11 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Article;
-use App\Entity\Author;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
 class ArticleController extends AbstractController
 {
     /**
@@ -24,9 +18,8 @@ class ArticleController extends AbstractController
             'articles' => $articles,
         ]);
     }
-
     /**
-     * @Route("/{id}", requirements={"id":"\d+"}, methods={"GET"})
+     * @Route("/{id}", requirements={"id":"\d+"}, methods="GET")
      */
     public function show(Article $article)
     {
@@ -34,7 +27,6 @@ class ArticleController extends AbstractController
             'article' => $article,
         ]);
     }
-
     /**
      * @Route("/new", methods={"GET", "POST"})
      */
@@ -42,55 +34,45 @@ class ArticleController extends AbstractController
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article, [
-            'validation_groups' => ['new', 'Default']
+            'validation_groups' => ['new', 'Default'],
         ]);
-
         $form->handleRequest($request);
-
-        if (($form)->isSubmitted() && $form->isValid()){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($article);
-            $em->flush();
-
-            $this->addFlash('success', 'le nouvel article a bien été créé');
-
-            return $this->redirectToRoute('app_article_show', [
-                'id' => $article->getId(),
-            ]);
-    }
-
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->persistArticle($article, 'Le nouvel article a bien été créé');
+        }
         return $this->render('article/new.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-
     /**
-     * @Route ("/{id}/edit", requirements={"id": "\d+"}, methods={"GET", "POST"} )
+     * @Route(
+     *     "/{id}/edit",
+     *     requirements={"id": "\d+"},
+     *     methods={"GET", "POST"}
+     * )
      */
     public function edit(Request $request, Article $article)
     {
         $form = $this->createForm(ArticleType::class, $article, [
             'full' => false,
         ]);
-
         $form->handleRequest($request);
-
-        if (($form)->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($article);
-            $em->flush();
-
-            $this->addFlash('success', 'l\'article a bien été modifié');
-
-            return $this->redirectToRoute('app_article_show', [
-                'id' => $article->getId(),
-            ]);
-
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->persistArticle($article, 'L\'article a bien été modifié');
         }
-
         return $this->render('article/edit.html.twig', [
-        'form' => $form->createView(),
-        'article' => $article,
+            'form' => $form->createView(),
+            'article' => $article,
+        ]);
+    }
+    private function persistArticle(Article $article, string $message)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+        $this->addFlash('success', $message);
+        return $this->redirectToRoute('app_article_show', [
+            'id' => $article->getId(),
         ]);
     }
 }
